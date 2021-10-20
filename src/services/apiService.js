307c8d526ad0ef3c9cd9264866999adfaedaa648
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-10-21 00:50:28 +0300

    fixApiService

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -8,7 +8,7 @@ export default class PicsApiService {
   fetchPictures() {
     const API_KEY = '23134043-5454bb2c28435773c6d2778ec';
     const BASE_URL = 'https://pixabay.com/api/';
-    const URL = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+    const URL = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=15&key=${API_KEY}`;
     return fetch(URL)
       .then(response => response.json())
       .then(data => {
